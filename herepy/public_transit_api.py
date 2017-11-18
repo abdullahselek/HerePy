6a6bf342cb3ab84df55e607d36a848532c7278b2
Abdullah Selek <abdullahselek@gmail.com>
2017-11-18 20:58:35 +0100

    add private get function

--- herepy/public_transit_api.py
+++ herepy/public_transit_api.py
@@ -9,6 +9,7 @@ import requests
 from herepy.here_api import HEREApi
 from herepy.utils import Utils
 from herepy.error import HEREError
+from herepy.models import PublicTransitResponse
 
 class PublicTransitApi(HEREApi):
     """A python interface into the HERE Public Transit API"""
@@ -26,3 +27,12 @@ class PublicTransitApi(HEREApi):
 
         super(PublicTransitApi, self).__init__(app_id, app_code, timeout)
         self._base_url = 'https://cit.transit.api.here.com/v3/stations/'
+
+    def __get(self, data):
+        url = Utils.build_url(self._base_url, extra_params=data)
+        response = requests.get(url, timeout=self._timeout)
+        json_data = json.loads(response.content.decode('utf8'))
+        if json_data.get('Res') != None:
+            return PublicTransitResponse.new_from_jsondict(json_data)
+        else:
+            return HEREError(json_data.get('details', 'Error occured on ' + sys._getframe(1).f_code.co_name))
