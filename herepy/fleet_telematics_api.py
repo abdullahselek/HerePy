315c8ed5513e9169b06f1f7174c43b879a75cce8
Abdullah Selek <abdullahselek@gmail.com>
2020-05-25 21:50:37 +0100

    Create a helper method that create params to find sequence

--- herepy/fleet_telematics_api.py
+++ herepy/fleet_telematics_api.py
@@ -4,27 +4,10 @@ import sys
 import json
 import requests
 
+from typing import List
+
 from herepy.here_api import HEREApi
-
-
-class FleetTelematicsApi(HEREApi):
-    """A python interface into the HERE Fleet Telematics API"""
-
-    def __init__(self,
-                 api_key: str=None,
-		 timeout: int=None):
-        """Returns a FleetTelematicsApi instance.
-        Args:
-          api_key (str):
-            API key taken from HERE Developer Portal.
-          timeout (int):
-            Timeout limit for requests.
-        Returns:
-          FleetTelematicsApi instance.
-        """
-
-        super(FleetTelematicsApi, self).__init__(api_key, timeout)
-        self._base_url = 'https://wse.ls.hereapi.com/2/'
+from herepy.here_enum import RouteMode
 
 
 class DestinationParam(object):
@@ -53,3 +36,48 @@ class DestinationParam(object):
 
     def __str__(self):
         return str.format('{0};{1},{2}', self.text, self.latitude, self.longitude)
+
+
+class FleetTelematicsApi(HEREApi):
+    """A python interface into the HERE Fleet Telematics API"""
+
+    def __init__(self,
+                 api_key: str=None,
+		 timeout: int=None):
+        """Returns a FleetTelematicsApi instance.
+        Args:
+          api_key (str):
+            API key taken from HERE Developer Portal.
+          timeout (int):
+            Timeout limit for requests.
+        Returns:
+          FleetTelematicsApi instance.
+        """
+
+        super(FleetTelematicsApi, self).__init__(api_key, timeout)
+        self._base_url = 'https://wse.ls.hereapi.com/2/'
+
+
+    def create_find_sequence_parameters(self,
+                                        start: DestinationParam,
+                                        intermediate_destinations: List[DestinationParam],
+                                        end: DestinationParam,
+                                        modes: [RouteMode]):
+        data = {'apiKey': self._api_key,
+                'start': start.__str__()}
+
+        count = 0
+        for destination_param in intermediate_destinations:
+            count += 1
+            data[str.format('destination{0}', count)] = destination_param.__str__()
+
+        data['end'] = end.__str__()
+        data['improveFor'] = 'time'
+        data['departure'] = 'now'
+
+        modes_str = ''
+        for route_mode in modes:
+            modes_str += route_mode.__str__() + ';'
+
+        data['mode'] = modes_str
+        return data
--- tests/test_fleet_telematics_api.py
+++ tests/test_fleet_telematics_api.py
@@ -7,6 +7,7 @@ import json
 import responses
 import herepy
 
+
 class FleetTelematicsApiTest(unittest.TestCase):
 
     def setUp(self):
@@ -26,3 +27,35 @@ class FleetTelematicsApiTest(unittest.TestCase):
                                                     longitude=8.6647)
         param_str = destination_param.__str__()
         self.assertEqual(param_str, 'FranfurtCentralStation;50.1073,8.6647')
+
+
+    def test_create_find_sequence_parameters(self):
+        start = herepy.DestinationParam(text='WiesbadenCentralStation',
+                                        latitude=50.0715,
+                                        longitude=8.2434)
+        intermediate_destinations = [herepy.DestinationParam(text='FranfurtCentralStation',
+                                                             latitude=50.1073,
+                                                             longitude=8.6647)]
+        intermediate_destinations.append(herepy.DestinationParam(text='DarmstadtCentralStation',
+                                                                 latitude=49.8728,
+                                                                 longitude=8.6326))
+        intermediate_destinations.append(herepy.DestinationParam(text='FrankfurtAirport',
+                                                                 latitude=50.0505,
+                                                                 longitude=8.5698))
+        end = herepy.DestinationParam(text='MainzCentralStation',
+                                      latitude=50.0021,
+                                      longitude=8.259)
+        modes = [herepy.RouteMode.fastest, herepy.RouteMode.car, herepy.RouteMode.traffic_enabled]
+        data = self._api.create_find_sequence_parameters(start=start,
+                                                         intermediate_destinations=intermediate_destinations,
+                                                         end=end,
+                                                         modes=modes)
+        self.assertEqual(data, {'apiKey': 'api_key',
+                                'start': 'WiesbadenCentralStation;50.0715,8.2434',
+                                'destination1': 'FranfurtCentralStation;50.1073,8.6647',
+                                'destination2': 'DarmstadtCentralStation;49.8728,8.6326',
+                                'destination3': 'FrankfurtAirport;50.0505,8.5698',
+                                'end': 'MainzCentralStation;50.0021,8.259',
+                                'improveFor': 'time',
+                                'departure': 'now',
+                                'mode' : 'fastest;car;traffic:enabled;'})
