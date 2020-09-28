64198fb26692ac646a9fd379e73db8cb4ad9d570
Abdullah Selek <abdullahselek@gmail.com>
2020-09-28 21:18:33 +0100

    Get incidents in a bounding box

--- herepy/traffic_api.py
+++ herepy/traffic_api.py
@@ -8,6 +8,9 @@ from herepy.here_api import HEREApi
 from herepy.utils import Utils
 from herepy.error import HEREError, UnauthorizedError, InvalidRequestError
 from herepy.models import TrafficIncidentResponse
+from herepy.here_enum import IncidentsCriticality
+
+from typing import List, Optional
 
 
 class TrafficApi(HEREApi):
@@ -25,11 +28,11 @@ class TrafficApi(HEREApi):
         """
 
         super(TrafficApi, self).__init__(api_key, timeout)
-        self._base_url = 'https://traffic.ls.hereapi.com/traffic/6.0'
+        self._base_url = 'https://traffic.ls.hereapi.com/traffic/6.0/'
 
 
-    def _get(self, data):
-        url = Utils.build_url(self._base_url, extra_params=data)
+    def __get(self, url, data):
+        url = Utils.build_url(url, extra_params=data)
         response = requests.get(url, timeout=self._timeout)
         json_data = json.loads(response.content.decode('utf8'))
         if json_data.get('TRAFFICITEMS') != None:
@@ -37,11 +40,11 @@ class TrafficApi(HEREApi):
                 json_data, param_defaults={'TRAFFICITEMS': None}
             )
         else:
-            error = self._get_error_from_response(json_data)
+            error = self.__get_error_from_response(json_data)
             raise error
 
 
-    def _get_error_from_response(self, json_data):
+    def __get_error_from_response(self, json_data):
         if "error" in json_data:
             if json_data["error"] == "Unauthorized":
                 return UnauthorizedError(json_data["error_description"])
@@ -53,3 +56,32 @@ class TrafficApi(HEREApi):
             return InvalidRequestError(error_message)
         else:
             return HEREError(error_message)
+
+
+    def __prepare_criticality_values(self, criticality_enums: [IncidentsCriticality]):
+        criticality_values = ""
+        for criticality in criticality_enums:
+            criticality_values += criticality.__str__() + ','
+        criticality_values = criticality_values[:-1]
+        return criticality_values
+
+
+    def incidents_in_bounding_box(self, top_left: List[float],
+                    bottom_right: List[float], criticality: [IncidentsCriticality]) -> Optional[TrafficIncidentResponse]:
+        """Request traffic incident information within specified area.
+        Args:
+          top_left (array):
+            array including latitude and longitude in order.
+          bottom_right (array):
+            array including latitude and longitude in order.
+          criticality (array):
+            List of IncidentsCriticality.
+        Returns:
+          TrafficIncidentResponse
+        Raises:
+          HEREError"""
+
+        data = {'bbox': str.format('{0},{1};{2},{3}', top_left[0], top_left[1], bottom_right[0], bottom_right[1]),
+                'apiKey': self._api_key,
+                'criticality': self.__prepare_criticality_values(criticality_enums=criticality)}
+        return self.__get(self._base_url + 'incidents.json', data)
--- tests/test_traffic_api.py
+++ tests/test_traffic_api.py
@@ -14,6 +14,30 @@ class TrafficApiTest(unittest.TestCase):
         api = herepy.TrafficApi('api_key')
         self._api = api
 
+
     def test_initiation(self):
         self.assertIsInstance(self._api, herepy.TrafficApi)
         self.assertEqual(self._api._api_key, 'api_key')
+
+
+    @responses.activate
+    def test_incidents_in_bounding_box_success(self):
+        with codecs.open('testdata/models/traffic_incidents_bounding_box.json', mode='r', encoding='utf-8') as f:
+            expectedResponse = f.read()
+        responses.add(responses.GET, 'https://traffic.ls.hereapi.com/traffic/6.0/incidents.json',
+                  expectedResponse, status=200)
+        response = self._api.incidents_in_bounding_box(top_left=[52.5311, 13.3644], bottom_right=[52.5114, 13.4035],
+                            criticality=[herepy.here_enum.IncidentsCriticality.minor, herepy.here_enum.IncidentsCriticality.major, herepy.here_enum.IncidentsCriticality.critical])
+        self.assertTrue(response)
+        self.assertIsInstance(response, herepy.TrafficIncidentResponse)
+
+
+    @responses.activate
+    def test_incidents_in_bounding_box_fails(self):
+        with codecs.open('testdata/models/traffic_incidents_error.json', mode='r', encoding='utf-8') as f:
+            expectedResponse = f.read()
+        responses.add(responses.GET, 'https://traffic.ls.hereapi.com/traffic/6.0/incidents.json',
+                  expectedResponse, status=200)
+        with self.assertRaises(herepy.UnauthorizedError):
+            self._api.incidents_in_bounding_box(top_left=[52.5311, 13.3644], bottom_right=[52.5114, 13.4035],
+                    criticality=[herepy.here_enum.IncidentsCriticality.minor])
