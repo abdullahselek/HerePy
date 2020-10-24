34fd2ee10dd256dc928391bcaf01441293a8d713
Abdullah Selek <abdullahselek@gmail.com>
2020-10-24 20:19:54 +0100

    Start adding separate examples

---
+++ examples/destination_weather_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from herepy import DestinationWeatherApi
+from herepy.here_enum import WeatherProductType
+
+# weather conditions with given location name
+destination_weather_api = DestinationWeatherApi("api_key")
+response = destination_weather_api.weather_for_location_name(
+    location_name="Berlin", product=WeatherProductType.forecast_7days
+)
+print(response.as_dict())
+
+# weather conditions within given coordinates
+response = destination_weather_api.weather_for_coordinates(
+    latitude=52.51784,
+    longitude=13.38736,
+    product=WeatherProductType.forecast_7days,
+)
+print(response.as_dict())
+
+# weather conditions within given zipcode
+response = destination_weather_api.weather_for_zip_code(
+    zip_code="10025", product=WeatherProductType.forecast_7days
+)
+print(response.as_dict())
