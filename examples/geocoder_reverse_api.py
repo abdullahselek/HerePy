f6318241a108d2a21b1f4f384ea5d3cd1714833c
Abdullah Selek <abdullahselek@gmail.com>
2020-10-26 19:43:36 +0000

    Add examples for GeocoderReverseApi

---
+++ examples/geocoder_reverse_api.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from herepy import GeocoderReverseApi
+
+geocoder_reverse_api = GeocoderReverseApi(api_key="api_key")
+
+# fetches the addresses information of a point
+response = geocoder_reverse_api.retrieve_addresses(
+    prox=[41.8842, -87.6388], limit=3
+)
+print(response.as_dict())
