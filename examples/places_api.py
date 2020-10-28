6db583b3ea920ae38aed48d230f5b97a6f630b6d
Abdullah Selek <abdullahselek@gmail.com>
2020-10-28 18:53:16 +0000

    Add examples for PlacesApi

---
+++ examples/places_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from herepy import PlacesApi
+
+places_api = PlacesApi(api_key="api_key")
+
+# fetches a list of places based on a query string
+response = places_api.onebox_search(
+    coordinates=[37.7905, -122.4107], query="restaurant"
+)
+print(response.as_dict())
+
+# fetches a list of places based on a query string and country code
+response = places_api.search_in_country(
+    coordinates=[37.7905, -122.4107], query="cafe", country_code="USA"
+)
+print(response.as_dict())
+
+# a list of popular places around a location
+response = places_api.places_in_circle(
+    coordinates=[37.7905, -122.4107], radius=1000, query="cafe"
+)
+print(response.as_dict())
