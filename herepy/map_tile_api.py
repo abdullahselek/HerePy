68ef2e7b1a35e6d2e208120e969117e75ba5b17d
Abdullah Selek <abdullahselek@gmail.com>
2021-01-26 22:23:41 +0000

    Add get map tile method

--- herepy/map_tile_api.py
+++ herepy/map_tile_api.py
@@ -5,14 +5,21 @@ import json
 import requests
 
 from random import randrange
+from typing import Dict, Optional
 from herepy.here_api import HEREApi
-from herepy import MapTileApiType
+from herepy.utils import Utils
+from herepy import (
+    MapTileApiType,
+    BaseMapTileResourceType,
+    AerialMapTileResourceType,
+    TrafficMapTileResourceType,
+)
 
 
 class MapTileApi(HEREApi):
     """A python interface into the HERE Map Tile API"""
 
-    def __init__(self, api_key: str = None, api_type: MapTileApiType = MapTileApiType.base, timeout: int = None):
+    def __init__(self, api_key: str = None, timeout: int = None):
         """Returns a MapTileApi instance.
         Args:
           api_key (str):
@@ -24,6 +31,39 @@ class MapTileApi(HEREApi):
         """
 
         super(MapTileApi, self).__init__(api_key, timeout)
-        server = randrange(1, 4)
-        self._base_url = str.format("https://{}.{}.maps.ls.hereapi.com/maptile/2.1/", server, api_type.__str__())
+        self._base_url = None
 
+    def get_maptile(
+        self,
+        api_type: MapTileApiType = MapTileApiType.base,
+        resource_type: BaseMapTileResourceType = BaseMapTileResourceType.alabeltile,
+        map_id: str = "newest",
+        scheme: str = "normal.day",
+        zoom: int = 13,
+        column: int = 4400,
+        row: int = 2686,
+        size: int = 256,
+        tile_format: str = "png8",
+        query_parameters: Optional[Dict] = None,
+    ):
+        server = randrange(1, 4)
+        url = str.format(
+            "https://{}.{}.maps.ls.hereapi.com/maptile/2.1/{}/{}/{}/{}/{}/{}/{}/{}",
+            server,
+            api_type.__str__(),
+            resource_type.__str__(),
+            map_id,
+            scheme,
+            zoom,
+            column,
+            row,
+            size,
+            tile_format,
+        )
+        if query_parameters:
+            query_parameters.update({"apiKey": self._api_key})
+        else:
+            query_parameters = {"apiKey": self._api_key}
+        url = Utils.build_url(url, extra_params=query_parameters)
+        response = requests.get(url, timeout=self._timeout, stream=True)
+        return response.content
--- tests/test_map_tile_api.py
+++ tests/test_map_tile_api.py
@@ -4,18 +4,33 @@ import os
 import sys
 import io
 import unittest
-import responses
 import codecs
 
+from unittest.mock import Mock, patch
 from herepy import MapTileApi, MapTileApiType
 
 
 class MapTileApiTest(unittest.TestCase):
     def setUp(self):
-        api = MapTileApi(api_key="api_key", api_type=MapTileApiType.base)
+        api = MapTileApi(api_key="api_key")
         self._api = api
 
     def test_initiation(self):
         self.assertIsInstance(self._api, MapTileApi)
         self.assertEqual(self._api._api_key, "api_key")
-        self.assertIsNotNone(self._api._base_url)
+        self.assertIsNone(self._api._base_url)
+
+    @patch("herepy.map_tile_api.requests.get")
+    def test_get_map_tile_with_default_parameters_succeed(self, mock_get):
+        with open("testdata/tiles/berlin.png", "rb") as tile:
+            mock_get.return_value = Mock(ok=True)
+            mock_get.return_value.content = tile.read
+        map_tile = self._api.get_maptile()
+        self.assertIsNotNone(map_tile)
+
+    @patch("herepy.map_tile_api.requests.get")
+    def test_get_map_tile_with_default_parameters_fails(self, mock_get):
+        mock_get.return_value = Mock(ok=True)
+        mock_get.return_value.content = None
+        map_tile = self._api.get_maptile()
+        self.assertIsNone(map_tile)
