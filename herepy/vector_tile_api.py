6b32d78bd530948fa978cfb5e64668226c579d12
Abdullah Selek <abdullahselek@gmail.com>
2021-01-29 21:11:11 +0000

    Add get vector tile method

--- herepy/vector_tile_api.py
+++ herepy/vector_tile_api.py
@@ -4,7 +4,10 @@ import sys
 import json
 import requests
 
+from typing import Optional, Dict
 from herepy.here_api import HEREApi
+from herepy.utils import Utils
+from herepy import VectorMapTileLayer
 
 
 class VectorTileApi(HEREApi):
@@ -21,3 +24,37 @@ class VectorTileApi(HEREApi):
 
         super(VectorTileApi, self).__init__(api_key, timeout)
         self._base_url = "https://vector.hereapi.com/v2/vectortiles/"
+
+    def get_vectortile(
+        self,
+        x: int,
+        y: int,
+        layer: VectorMapTileLayer = VectorMapTileLayer.base,
+        projection: str = "mc",
+        zoom: int = 11,
+        tile_format: str = "omv",
+        query_parameters: Optional[Dict] = None):
+        url = str.format(
+            self._base_url + "{}/{}/{}/{}/{}/{}",
+            layer.__str__(),
+            projection,
+            zoom,
+            x,
+            y,
+            tile_format,
+        )
+        if query_parameters:
+            query_parameters.update({"apiKey": self._api_key})
+        else:
+            query_parameters = {"apiKey": self._api_key}
+        url = Utils.build_url(url, extra_params=query_parameters)
+        response = requests.get(url, timeout=self._timeout, stream=True)
+        if isinstance(response.content, bytes):
+            try:
+                json_data = json.loads(response.content.decode("utf8"))
+                if "error" in json_data:
+                    error = self.__get_error_from_response(json_data)
+                    raise error
+            except UnicodeDecodeError as err:
+                print("Vector tile downloaded")
+        return response.content
--- tests/test_vector_tile_api.py
+++ tests/test_vector_tile_api.py
@@ -7,6 +7,7 @@ import unittest
 import codecs
 import json
 
+from unittest.mock import Mock, patch
 from herepy import VectorTileApi
 
 
@@ -19,3 +20,18 @@ class VectorTileApiTest(unittest.TestCase):
         self.assertIsInstance(self._api, VectorTileApi)
         self.assertEqual(self._api._api_key, "api_key")
         self.assertEqual(self._api._base_url, "https://vector.hereapi.com/v2/vectortiles/")
+
+    @patch("herepy.vector_tile_api.requests.get")
+    def test_get_vector_tile_succeed(self, mock_get):
+        with open("testdata/tiles/omv", "rb") as tile:
+            mock_get.return_value = Mock(ok=True)
+            mock_get.return_value.content = tile.read
+        vector_tile = self._api.get_vectortile(x=1100, y=671)
+        self.assertIsNotNone(vector_tile)
+
+    @patch("herepy.vector_tile_api.requests.get")
+    def test_get_vector_tile_fails(self, mock_get):
+        mock_get.return_value = Mock(ok=True)
+        mock_get.return_value.content = None
+        vector_tile = self._api.get_vectortile(x=1100, y=671)
+        self.assertIsNone(vector_tile)
