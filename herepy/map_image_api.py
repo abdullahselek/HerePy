b704c3305f078940c6ecfec0af060039c66dc364
Abdullah Selek <abdullahselek@gmail.com>
2021-02-08 19:32:28 +0000

    Start implementing map image api client

--- herepy/map_image_api.py
+++ herepy/map_image_api.py
@@ -4,7 +4,11 @@ import sys
 import json
 import requests
 
+from typing import List, Optional
 from herepy.here_api import HEREApi
+from herepy.utils import Utils
+from herepy import MapImageResourceType
+from herepy.error import HEREError, InvalidRequestError, UnauthorizedError
 
 
 class MapImageApi(HEREApi):
@@ -21,3 +25,57 @@ class MapImageApi(HEREApi):
 
         super(MapImageApi, self).__init__(api_key, timeout)
         self._base_url = "https://image.maps.ls.hereapi.com/mia/1.6/mapview"
+
+    def __get_error_from_response(self, json_data):
+        if "error" in json_data:
+            error_description = json_data["error_description"]
+            if json_data["error"] == "Unauthorized":
+                return UnauthorizedError(error_description)
+        error_type = json_data.get("Type")
+        error_message = json_data.get(
+            "Message",
+            error_description + ", error occured on " + sys._getframe(1).f_code.co_name,
+        )
+        if error_type == "Invalid Request":
+            return InvalidRequestError(error_message)
+        else:
+            return HEREError(error_message)
+
+    def get_mapimage(
+        self,
+        coordinates: List[float],
+        zoom: int = 8,
+        uncertainty: Optional[str] = None,
+    ):
+        """Retrieves the map image with given parameters.
+        Args:
+          coordinates (List):
+            List contains latitude and longitude in order.
+          zoom (int):
+            Zoom level for the map image.
+          uncertainty (Optional[str]):
+            The parameter u specifies position uncertainty, which is shown as a filled circle around a
+            location defined in terms of its latitude and longitude. The value of the parameter u indicates
+            the radius of the circle representing uncertainty. In this case, the radius is set to 5 myriameters,
+            which is 50000 meters.
+        Returns:
+          Map image as bytes.
+        """
+        data = {
+            "c": str.format("{0},{1}", coordinates[0], coordinates[1]),
+            "z": zoom,
+            "apiKey": self._api_key,
+        }
+        if uncertainty:
+            data["u"] = uncertainty
+        url = Utils.build_url(self._base_url, extra_params=data)
+        response = requests.get(url, timeout=self._timeout)
+        if isinstance(response.content, bytes):
+            try:
+                json_data = json.loads(response.content.decode("utf8"))
+                if "error" in json_data:
+                    error = self.__get_error_from_response(json_data)
+                    raise error
+            except UnicodeDecodeError as err:
+                print("Map image downloaded")
+        return response.content
--- tests/test_map_image_api.py
+++ tests/test_map_image_api.py
@@ -19,4 +19,29 @@ class MapImageApiTest(unittest.TestCase):
     def test_initiation(self):
         self.assertIsInstance(self._api, MapImageApi)
         self.assertEqual(self._api._api_key, "api_key")
-        self.assertEqual(self._api._base_url, "https://image.maps.ls.hereapi.com/mia/1.6/mapview")
+        self.assertEqual(
+            self._api._base_url, "https://image.maps.ls.hereapi.com/mia/1.6/mapview"
+        )
+
+    @patch("herepy.map_image_api.requests.get")
+    def test_get_mapimage_when_succeed(self, mock_get):
+        with open("testdata/images/new-delhi-uncertainty.jpg", "rb") as f:
+            mock_get.return_value = Mock(ok=True)
+            byte_im = f.read()
+            mock_get.return_value.content = byte_im
+        vector_tile = self._api.get_mapimage(
+            coordinates=[28.371425, 77.387695],
+            uncertainty="5m",
+        )
+        self.assertIsNotNone(vector_tile)
+        self.assertTrue(isinstance(vector_tile, bytes))
+
+    @patch("herepy.map_image_api.requests.get")
+    def test_get_mapimage_when_fails(self, mock_get):
+        mock_get.return_value = Mock(ok=True)
+        mock_get.return_value.content = None
+        vector_tile = self._api.get_mapimage(
+            coordinates=[28.371425, 77.387695],
+            uncertainty="5m",
+        )
+        self.assertIsNone(vector_tile)
