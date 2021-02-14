c2d47004c189e32853f0162a3f019fd678b228fe
Abdullah Selek <abdullahselek@gmail.com>
2021-02-14 21:05:26 +0000

    Add more parameters

--- herepy/map_image_api.py
+++ herepy/map_image_api.py
@@ -7,7 +7,7 @@ import requests
 from typing import List, Optional
 from herepy.here_api import HEREApi
 from herepy.utils import Utils
-from herepy import MapImageResourceType
+from herepy import MapImageResourceType, MapImageFormatType
 from herepy.error import HEREError, InvalidRequestError, UnauthorizedError
 
 
@@ -51,6 +51,13 @@ class MapImageApi(HEREApi):
         center: List[float] = None,
         encoded_geo_coordinate: str = None,
         encoded_geo_center_coordinate: str = None,
+        image_format: MapImageFormatType = MapImageFormatType.png,
+        image_height: Optional[int] = None,
+        show_position: bool = False,
+        maxhits: int = 1,
+        label_language: str = "eng",
+        second_label_language: Optional[str] = None,
+        house_number: Optional[str] = None,
         zoom: int = 8,
         map_scheme: Optional[int] = None,
         uncertainty: Optional[str] = None,
@@ -70,9 +77,9 @@ class MapImageApi(HEREApi):
             Note: If this parameter is provided then the geo search parameters are ignored, such as co.
           coordinates (List[float]):
             List contains latitude and longitude in order.
-          city_name (str):
+          city_name (Optional[str]):
             City name for address based search. UTF-8 encoded and URL-encoded.
-          country_name (str):
+          country_name (Optional[str]):
             Country name for address based search. UTF-8 encoded and URL-encoded.
           center (List[float]):
             Map center point geo coordinate. If the position is on the border of the map, the dot might be cropped.
@@ -80,6 +87,33 @@ class MapImageApi(HEREApi):
             Encoded equivalent of position geo coordinate parameter c. Parameter c is ignored if this parameter is specified.
           encoded_geo_center_coordinate (str):
             Encoded equivalent of map center point geo coordinate parameter ctr. Parameter ctr is ignored if this parameter is present.
+          image_format (MapImageFormatType):
+            Image format. It is possible to request the map image.
+          image_height (Optional[int]):
+            Result image height in pixels, maximum 2048. Height and width parameter can be provided independently,
+            i.e. there is no need to enter both to resize the image.
+          show_position (bool):
+            Flag for showing address or position information box inside the map image
+            (if address is available or position is allowed to be shown).
+            Note: If geo search parameters such as co are provided, then the information shown
+            is related to those parameter's values, if valid.
+          maxhits (int):
+            Maximum number of search results to return. Applies only when some kind of search
+            is performed which can return multiple results. Set to 1 to show directly the first
+            matching result without any results listing.
+          label_language (str):
+            Map label language. Specifies the language to be used to display descriptions of details inside the map image.
+            If the parameter is not provided, the default language depends on the highest prioritized locale of the
+            client's Accept-Language HTTP header which is currently supported.
+            If no map language based on HTTP header can be determined, the server configured default is used.
+            Note: Some MARC three-letter language codes are supported, please check https://developer.here.com/documentation/map-image/dev_guide/topics/resource-map.html
+            for more details.
+          second_label_language (Optional[str]):
+            Second language to be used, only for dual labelling, therefore a ml language must also be present Map label language.
+            Note: Some MARC three-letter language codes are supported, please check https://developer.here.com/documentation/map-image/dev_guide/topics/resource-map.html
+            for more details.
+          house_number (Optional[str]):
+            House number on the street for address based search.
           zoom (int):
             Zoom level for the map image.
           map_scheme (Optional[int]):
@@ -124,6 +158,16 @@ class MapImageApi(HEREApi):
             data["u"] = uncertainty
         if nodot:
             data["nodot"] = None
+        if image_height:
+            data["h"] = image_height
+        if house_number:
+            data["n"] = house_number
+        data["f"] = image_format._value_
+        data["i"] = show_position
+        data["maxhits"] = maxhits
+        data["ml"] = label_language
+        if second_label_language:
+            data["ml2"] = second_label_language
         url = Utils.build_url(self._base_url, extra_params=data)
         response = requests.get(url, timeout=self._timeout)
         if isinstance(response.content, bytes):
