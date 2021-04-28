61d6bf2adaf5f5441a3d987653dc9da97348211f
Abdullah Selek <abdullahselek@gmail.com>
2021-04-28 19:57:21 +0100

    Add documentation

--- herepy/objects.py
+++ herepy/objects.py
@@ -13,6 +13,20 @@ class AvoidArea(object):
         east: int,
         avoid_type: str = "boundingBox",
     ):
+        """Returns a RoutingApi instance.
+        Args:
+          north (int):
+            Latitude in WGS-84 degrees of the northern boundary of the box.
+          south (int):
+            Latitude in WGS-84 degrees of the southern boundary of the box.
+          west (int):
+            Longitude in WGS-84 degrees of the western boundary of the box.
+          east (int):
+            Longitude in WGS-84 degrees of the eastern boundary of the box.
+          avoid_type (str):
+            Type of avoid, default `boundingBox`.
+        """
+
         self.north = north
         self.south = south
         self.west = west
@@ -24,5 +38,13 @@ class Avoid(object):
     """Class that helps RoutingAPI to avoid routes that violate the properties it has."""
 
     def __init__(self, features: List[AvoidFeature], areas: List[AvoidArea]):
+        """Returns a RoutingApi instance.
+        Args:
+          features (List[AvoidFeature]):
+            List of routing features to avoid during route calculation.
+          areas (List[AvoidArea]):
+            List of areas to avoid.
+        """
+
         self.features = features
         self.areas = areas
